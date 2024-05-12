813c93cc55db3794a92876fa12c56f08dde4777c
krokodila888 <krokodila888@yandex.ru>
2024-05-12 20:56:55 +0300

    FIX: correct ways to scss vatiables

--- src/services/middleware.ts
+++ src/services/middleware.ts
@@ -2,13 +2,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import type { Middleware, MiddlewareAPI } from "redux";
 import type { ThunkDispatch } from "redux-thunk";
-import { useDispatch, TypedUseSelectorHook, useSelector } from "react-redux";
+import { TypedUseSelectorHook, useSelector } from "react-redux";
 // eslint-disable-next-line import/namespace
 import { store, TAppActions } from "./store";
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = ThunkDispatch<RootState, unknown, TAppActions>;
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+//export const useAppDispatch = () => useAppDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export const middleware1 = (): Middleware => {
